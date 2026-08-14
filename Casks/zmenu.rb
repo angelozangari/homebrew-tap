7771f13e2fcf00ade71928d7786bcbeaec1f3462
angelozangari <angelo.zangari10@gmail.com>
2026-08-14 15:15:50 +0200

    [zmenu] update to 0.1.2

--- Casks/zmenu.rb
+++ Casks/zmenu.rb
@@ -1,11 +1,11 @@
 cask "zmenu" do
-  version "0.1.1"
-  sha256 "abbd5651f6a1ce37854fb3b128c5e13dca910a1b71db097fcd15fdf93dfe5cab"
+  version "0.1.2"
+  sha256 "03d6d061d59dc8b3efa0ec7390c156f4149c79791010b358a4c5aaf6196fb03c"
 
-  url "https://github.com/angelozangari/homebrew-tap/releases/download/zmenu-v#{version}/zmenu-#{version}.zip"
+  url "https://github.com/angelozangari/homebrew-tap/releases/download/zmenu-v0.1.2/zmenu-0.1.2.zip"
   name "zmenu"
-  desc "Menu bar GUI for miscellaneous services"
-  homepage "https://github.com/angelozangari/homebrew-tap"
+  desc "Menu bar launcher for personal macOS utilities"
+  homepage "https://github.com/angelozangari/zmenu"
 
   app "zmenu.app"
 end
