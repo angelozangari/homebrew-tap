f31a98650eb590ba98815cb0ceb576729e1fe6c9
angelozangari <angelo.zangari10@gmail.com>
2026-08-14 15:16:28 +0200

    [zwallpaper] update to 0.1.2

---
+++ Casks/zwallpaper.rb
@@ -0,0 +1,11 @@
+cask "zwallpaper" do
+  version "0.1.2"
+  sha256 "118dfa15e4f06e0234c677ce17e091bee4bd6232b5172b82af1a3b5c6597188d"
+
+  url "https://github.com/angelozangari/homebrew-tap/releases/download/zwallpaper-v0.1.2/zwallpaper-0.1.2.zip"
+  name "zwallpaper"
+  desc "Native wallpaper picker for macOS"
+  homepage "https://github.com/angelozangari/zwallpaper"
+
+  app "zwallpaper.app"
+end
